1f3e115f024faef49b9271e9fe96c8c96956f05d
apurohit <abhishekay.purohit@anaplan.com>
2020-09-08 09:52:15 -0700

    added few things

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.7
 
-COPY boot.sh /init.sh
+COPY init.sh /init.sh
 RUN apk --no-cache add memcached && chmod +x /init.sh
 
 USER memcached
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/memcached \
+$(/usr/bin/memcached \
   --user=${MEMCACHED_USER:-memcached} \
   --listen=${MEMCACHED_HOST:-0.0.0.0} \
   --port=${MEMCACHED_PORT:-11211} \
@@ -8,4 +8,4 @@
   --conn-limit=${MEMCACHED_MAXCONN:-1024} \
   --threads=${MEMCACHED_THREADS:-4} \
   --max-reqs-per-event=${MEMCACHED_REQUESTS_PER_EVENT:-20} \
-  --verbose
+  --verbose)
